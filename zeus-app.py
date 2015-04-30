153c5bac2f43f96dc99641e10e68e7115475be08
kuhuang <kuhuang@cisco.com>
2015-04-30 15:03:15 -0700

    push data to zeus

--- zeus-app.py
+++ zeus-app.py
@@ -2,110 +2,37 @@ from zeus import ZeusClient
 import os
 import json
 import random
+import csv
+import time
 
+BATCH_AMOUNT = 5000
 ZEUS_API = "http://api.ciscozeus.io"
-BATCH_SIZE = 1000
-NUMBER_OF_SAMPLES = 1000
-auth_data = None
-
-
-# Interactive Zeus 101. Run python file to see it.
-os.system('clear')
-print("\nWELCOME to Zeus 101\n")
-print("This is a sample App that inserts and retrieves data into zeus.")
-print(
-    "For the purpose of this example, we will connect to zeus,\nwe will send " +
-    "some logs and metrics and finally, we will retrieve them.")
-
-raw_input("Press ENTER when you are ready to continue.")
-
-
-print("Connecting to zeus...")
-
-#token = raw_input("What is your user token? ")
 token = "68c5e168"
-
 z = ZeusClient(token, ZEUS_API)
-print("Zeus client created with user token " + token)
 
+# log_name = raw_input("please input the log_name:")
+log_name = "stackoverflow"
+sample_file = "StackOverflow/StackOverflow_20140116-20140122.csv"
 
-print("\nGreat! We are now ready to start sending and receiving data.")
+csvfile = open("data/"+sample_file)
+reader = csv.reader(csvfile)
 
-raw_input("Press ENTER when you are ready to continue.")
+header = reader.next()
 
-os.system('clear')
+logs = []
+for row in reader:
+    try:
+        timestamp = int(time.mktime(time.strptime(row[0], "%Y-%m-%d %H:%M:%S")))
+        message = row[1]
+        logs.append({"message": message, "timestamp": timestamp})
+    except:
+        print "invalid"
 
-print("User token: " + token)
+total = len(logs)
+num_package = total/BATCH_AMOUNT
+for i in range(num_package):
+    print 'Sending %d~%d'%(i*BATCH_AMOUNT, (i+1)*BATCH_AMOUNT)
+    print z.sendLog(log_name, logs[i*BATCH_AMOUNT:(i+1)*BATCH_AMOUNT])
 
-print("\nLets now post some logs and metrics.")
-print("We are going to send a log " +`NUMBER_OF_SAMPLES`+ " times in groups of " +`BATCH_SIZE`+ ".")
-message = raw_input("What message would you like to send? ")
-
-# Log sending
-print("\nPOST request to http://api.ciscozeus.io/logs/" + token + "/Zeus101")
-for i in range(0, NUMBER_OF_SAMPLES, BATCH_SIZE):
-    messages = [
-        {"message": message + " - " + str(i + x)} for x in range(0, min(NUMBER_OF_SAMPLES - i, BATCH_SIZE))]
-    print "Sending group " + str(i + 1) + ': ' + str(z.sendLog("Zeus101", messages))
-
-raw_input("\nLogs sent. Press ENTER to send some metrics.")
-
-# Metric sending
-print(
-    "\nPOST request to http://api.ciscozeus.io/metrics/" + token + "/Zeus101")
-for i in range(0, NUMBER_OF_SAMPLES, BATCH_SIZE):
-    metrics = [
-        {"value": random.randint(0, 100)} for x in range(0, min(NUMBER_OF_SAMPLES - i, BATCH_SIZE))]
-    print "Sending group " + str(i + 1) + ': ' + str(z.sendMetric("Zeus101", metrics))
-
-raw_input("\nMetrics sent. Press ENTER to continue.")
-
-os.system('clear')
-print("User token: " + token)
-print(
-    "\nCongratulations! You now have some logs and metrics in Zeus. Easy, right?")
-print("Lets now retrieve 10 logs from Zeus and see if they are what we sent.")
-raw_input("\nPress ENTER to get 10 logs.")
-os.system('clear')
-
-# Log consumption
-print(
-    "\nGET request to http://api.ciscozeus.io/logs/" + token + "?log_name=Zeus101&limit=10&pattern=" + message)
-status, l = z.getLog('Zeus101', limit=10, pattern=message)
-
-if status == 200:
-    print("\nLogs:")
-    for log in l['result']:
-        print `log['timestamp']` + ": " + log['message']
-    print "\nThere are currently " + str(l['total']) + " logs in your Zeus account that match this query."
-else:
-    print "\nThere has been an error retrieving logs. Are the parameters correctly formatted?"
-
-raw_input(
-    "\nLets now retrieve some metrics from Zeus. Press ENTER when you are ready.")
-
-# Metric consumption
-print(
-    "\nGET request to http://api.ciscozeus.io/metrics/" + token + "/_values?" +
-    "limit=10&group_interval=1m&aggregator_function=sum&filter_condition='value > 90'")
-status, m = z.getMetric(limit=10, group_interval='1m',
-                        aggregator='sum', filter_condition='value > 90')
-
-if status == 200:
-    print "\n" + m[0]['name'] + "\t" + '\t\t'.join(m[0]['columns'])
-    for metric in m[0]['points']:
-        print '\t' + '\t'.join(map(str, metric))
-else:
-    print "There has been an error retrieving metrics. Are the parameters correctly formatted?"
-
-raw_input(
-    "\nFinally, let's retrieve a list of metrics. Press ENTER when you are ready.")
-
-# Metric list consumption
-print("\nGET request to http://api.ciscozeus.io/metrics/" +
-      token + "/_names?limit=10")
-status, m = z.getMetricNames(limit=10)
-for metric in m:
-    print metric
-raw_input(
-    "\nThis is the end of Zeus101. Please, feel free to use the ZeusClient class as a base for your code. Press ENTER to exit.")
+print 'Sending %d~%d'%((i+1)*BATCH_AMOUNT, total)
+print z.sendLog(log_name, logs[(i+1)*BATCH_AMOUNT: total])
